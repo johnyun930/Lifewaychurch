703397b13378e8dee1f1a1f00f989ecb7d0905ab
unknown <johnyun930@gmail.com>
2021-02-28 12:26:29 -0800

    retry

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 export const db =()=>{
-     mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.umkpc.mongodb.net/${process.env.DB_NAME}`, {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
+     mongoose.connect(`mongodb+srv://johnyun930:taeyeon0309@cluster0.umkpc.mongodb.net/Lifeway`, {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
     console.log('connect to MongoDB');
 }).catch((err)=>{
     console.log(err);

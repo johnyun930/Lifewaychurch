ef933bd1ed354fa8ba88881c142246467a09e6ca
unknown <johnyun930@gmail.com>
2021-05-02 10:22:50 -0700

    localhost to heroku web server

--- build/db/index.js
+++ build/db/index.js
@@ -6,14 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = void 0;
 var mongoose_1 = __importDefault(require("mongoose"));
 exports.db = function () {
-    mongoose_1.default.connect("mongodb://localhost:27017/" + process.env.DB_NAME, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(function () {
-        console.log('connect to MongoDB');
-    }).catch(function (err) {
-        console.log(err);
-    });
-    //  mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME+":"+process.env.DB_PASSWORD}@cluster0.umkpc.mongodb.net/${process.env.DB_NAME}`, {useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true}).then(()=>{
+    //      mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`, {useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true}).then(()=>{
     //     console.log('connect to MongoDB');
     // }).catch((err)=>{
     //     console.log(err);
     // });
+    mongoose_1.default.connect("mongodb+srv://" + (process.env.DB_USERNAME + ":" + process.env.DB_PASSWORD) + "@cluster0.umkpc.mongodb.net/" + process.env.DB_NAME, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(function () {
+        console.log('connect to MongoDB');
+    }).catch(function (err) {
+        console.log(err);
+    });
 };
